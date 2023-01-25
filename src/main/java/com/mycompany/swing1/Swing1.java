c02adf5fa7f07e335567c0a2a5fbb3268040ecf6
sarkissian <sarkissian@localhost>
2023-01-25 11:31:02 +0100

    First commit

---
+++ src/main/java/com/mycompany/swing1/Swing1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swing1;
+
+/**
+ *
+ * @author sarkissian
+ */
+public class Swing1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
